a2c17123356bc7adb8da23312337f3e65adc064b
adrianlukito <lukito.adrian1@gmail.com>
2021-08-23 23:31:24 +0700

    DialogInput Test

--- app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/MainActivityTest.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/MainActivityTest.kt
@@ -8,6 +8,7 @@ import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.hamcrest.CoreMatchers.not
 import org.junit.Test
 
 import org.junit.runner.RunWith
@@ -15,33 +16,33 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest{
 
-
     @Test
     fun test_showDialog_captureNameInput() {
-
         // GIVEN
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        val NAME = "Mitch"
+        val EXPECTED_NAME = "adrian"
 
-        // Execute and Verify
+        // Excute and Verify
         onView(withId(R.id.button_launch_dialog)).perform(click())
 
-        onView(withText(R.string.text_enter_name)).check(matches(isDisplayed()))
+        onView(withText(R.string.text_enter_name))
+            .check(matches(isDisplayed()))
 
         onView(withText(R.string.text_ok)).perform(click())
 
-        // make sure dialog is still visible (can't click ok without entering a name)
-        onView(withText(R.string.text_enter_name)).check(matches(isDisplayed()))
+        onView(withText(R.string.text_enter_name))
+            .check(matches(isDisplayed()))
 
-        // enter a name
-        onView(withId(R.id.md_input_message)).perform(typeText(NAME))
+        // ENTER SOME INPUT
+        onView(withId(R.id.md_input_message)).perform(typeText(EXPECTED_NAME))
 
         onView(withText(R.string.text_ok)).perform(click())
 
-        // make sure dialog is gone
+        // makes sure dialog is gone
         onView(withText(R.string.text_enter_name)).check(doesNotExist())
 
-        onView(withId(R.id.text_name)).check(matches(withText(NAME)))
+        // confirm name is set to TextView in activity
+        onView(withId(R.id.text_name)).check(matches(withText(EXPECTED_NAME)))
     }
 }
 
