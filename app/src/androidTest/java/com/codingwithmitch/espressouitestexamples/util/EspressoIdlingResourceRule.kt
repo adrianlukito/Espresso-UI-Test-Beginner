70b80b062689541def5c0c99ad90d6764f261188
adrianlukito <lukito.adrian1@gmail.com>
2021-08-27 00:05:32 +0700

    Custom TestRules

--- app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/util/EspressoIdlingResourceRule.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/util/EspressoIdlingResourceRule.kt
@@ -2,20 +2,52 @@ package com.codingwithmitch.espressouitestexamples.util
 
 import androidx.test.espresso.IdlingRegistry
 import com.codingwithmitch.espressouitestingexamples.util.EspressoIdlingResource
+import org.junit.rules.TestRule
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
+import org.junit.runners.model.Statement
+import java.lang.Exception
 
+// Option 1
+// this option is much more difficult to read and is more verbose
+
+//class EspressoIdlingResourceRule: TestRule {
+//    override fun apply(base: Statement?, description: Description?): Statement {
+//        return IdlingResourceStatement(base)
+//    }
+//
+//    class IdlingResourceStatement(private val base: Statement?): Statement() {
+//        private val idlingResource = EspressoIdlingResource.countingIdlingResource
+//
+//        override fun evaluate() {
+//
+//            // Before
+//            IdlingRegistry.getInstance().register(idlingResource)
+//            try {
+//                base?.evaluate() ?: throw Exception("Error evaluating test. Base State is null")
+//            } finally {
+//                // After
+//                IdlingRegistry.getInstance().unregister(idlingResource)
+//            }
+//        }
+//    }
+//}
+
+// Option 2
+// Simplified version of option 1 (TestWatcher class implements TestRule)
 
 class EspressoIdlingResourceRule : TestWatcher(){
 
 
     private val idlingResource = EspressoIdlingResource.countingIdlingResource
 
+    // After
     override fun finished(description: Description?) {
         IdlingRegistry.getInstance().unregister(idlingResource)
         super.finished(description)
     }
 
+    // Before
     override fun starting(description: Description?) {
         IdlingRegistry.getInstance().register(idlingResource)
         super.starting(description)
